8ce01e7b347815d7a267ba5ce47506c53319d2ee
Eduardo Leal <eduardo.leeal@hotmail.com>
2025-04-16 15:43:34 -0300

    Finalizado Operadores logicos

---
+++ OperadoresLogicos/logica_and.dart
@@ -0,0 +1,10 @@
+void main(){
+  var a = 1;
+  var b = 2;
+
+  if(a > 0 && b < 3){
+    print('VERDADE');
+  } else {
+    print('FALSO');
+  }
+}
---
+++ OperadoresLogicos/logica_or.dart
@@ -0,0 +1,10 @@
+void main(){
+  var a = 1;
+  var b = 2;
+
+  if (a == 1 || b > 2){
+    print('VERDADE');
+  } else {
+    print('FALSO');
+  }
+}
